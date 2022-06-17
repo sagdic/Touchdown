6296e37e454ca8a8af1b9c3f58a09dfd05128eed
Tayfun Sağdıç <30289904+sagdic@users.noreply.github.com>
2022-06-17 10:41:33 +0300

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by tayfun on 17.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
